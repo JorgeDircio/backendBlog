e1d9f5b007f74de82170945b486ed4caabfae0b4
jorgedircio <jorgeluispedro16@gmail.com>
2023-08-12 01:10:34 -0600

    creacion de clase security constanst

---
+++ src/main/java/com/developerdircio/CrudCursos/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.developerdircio.CrudCursos.security;
+
+public class SecurityConstants {
+  public static final long EXPIRATION_DATE = 864000000; // 10 DIAS
+  public static final String TOKEN_PREFIX = "Bearer ";
+  public static final String HEADER_STRING = "Authorization";
+  public static final String SIGN_UP_URL = "/users";
+  public static final String TOKEN_SECRET = "4fwVfpPgmMWWByXNdQDZ51fUPgliNWWR";
+}
